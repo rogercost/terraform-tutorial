07603dd77ac42da5fce3a917bbde795eaf0fee8f
rcost <rcost@penguin>
2025-02-10 14:54:02 -0500

    add web server

--- main.tf
+++ main.tf
@@ -3,10 +3,35 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           =  "ami-018875e7376831abe"
+  ami           =  "ami-088b41ffb0933423f"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup python3 -m http.server 8080 --bind 0.0.0.0 &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
